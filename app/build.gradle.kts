ceb78c7337369969eacb331d92461f54f98666ea
Class_Student <Class_Student@rtk.local>
2024-05-13 13:41:36 +0300

    0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,11 +40,18 @@ android {
 
 dependencies {
 
+    implementation ("com.squareup.retrofit2:retrofit:2.3.0")
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.3.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.3.0")
+    implementation ("io.reactivex.rxjava2:rxandroid:2.0.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.contentpager:contentpager:1.0.0")
+    implementation("com.google.android.gms:play-services-basement:18.3.0")
+    implementation("com.google.android.gms:play-services-tflite-acceleration-service:16.0.0-beta01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/medapp/Url.kt
@@ -0,0 +1,8 @@
+package com.example.medapp
+
+class Url {
+    companion object {
+        const val BASE_URL = "mskko2021.mad.hakta.pro/api"
+        const val URL = "http://mskko2021.mad.hakta.pro/api"
+    }
+}
